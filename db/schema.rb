4d6e9c6af9cbdaf87f5ea7aec570de5a1a604464
Michael Ramuta <michaelramuta@gmail.com>
2017-03-29 22:24:53 -0500

    add approved boolean to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330031528) do
+ActiveRecord::Schema.define(version: 20170330032428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170330031528) do
     t.string   "username"
     t.string   "category"
     t.boolean  "explicit"
+    t.boolean  "approved"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
