5b99e317ec2edb73d6bcb909380ecdcaa59099d6
Michael Ramuta <michaelramuta@gmail.com>
2017-03-30 23:31:35 -0500

    add validations to episode

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,6 @@
 class Episode < ApplicationRecord
   belongs_to :user
+  validates_presence_of :user_id, :title, :subtitle, :summary, :audio_link, :image_link, :length, :duration
+
+  # before_save :validate_episode
 end
