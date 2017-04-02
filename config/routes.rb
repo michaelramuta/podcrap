f119bd590d5176f82f77208f367d19d27a1ed71f
Michael Ramuta <michaelramuta@gmail.com>
2017-04-02 14:22:28 -0500

    alias show users to show podcasts

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :episodes, only: [:edit, :update, :create, :new, :destroy]
 
   resources :help, only: [:index]
+
+  get '/users/:id' => 'podcasts#show'
 end
