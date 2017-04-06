3a7df78304abad2b7e43107fef4f46d87c7b3217
Michael Ramuta <michaelramuta@gmail.com>
2017-04-06 00:02:50 -0500

    add some episode validations

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,10 +1,18 @@
 class Episode < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :title, :subtitle, :summary, :audio_link, :image_link, :length, :duration
+  validate :duration_format, :length_format
   default_scope { order("created_at ASC") }
 
-  # before_save :validate_episode
-  # things to validate:
-  # length is formatted correctly
-  # duration is formatted correctly
+  def duration_format
+    if duration !~ /\d:\d\d/
+      errors.add(:duration, "Duration must be formatted as either HH:MM:SS, H:MM:SS, MM:SS, or M:SS")
+    end
+  end
+
+  def length_format
+    if length.to_i.to_s != length
+      errors.add(:length, "Its gotta be a number buddy, I can't believe I'm writing validations for this")
+    end
+  end
 end
