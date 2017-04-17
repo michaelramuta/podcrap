1621e475e8d9a29054e4a4f83792f54f4d1f36a1
Michael Ramuta <michaelramuta@gmail.com>
2017-04-17 00:57:14 -0500

    remove archive.org validations

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,7 @@
 class Episode < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :title, :subtitle, :summary, :audio_link, :image_link, :length, :duration
-  validate :duration_format, :length_format, :audio_link_with_archive_org
+  validate :duration_format, :length_format
   default_scope { order("created_at ASC") }
 
   def duration_format
@@ -14,17 +14,5 @@ class Episode < ApplicationRecord
     if length.to_i.to_s != length
       errors.add(:length, "Its gotta be a number buddy, I can't believe I'm writing validations for this")
     end
-  end
-
-  def audio_link_with_archive_org
-    unless audio_link.include?('archive.org')
-      errors.add(:audio_link, "Use archive.org for uploading episodes. More functionality coming soon")
-    end
-  end
-
-  def update_duration_and_length
-    # cut out the stuff from the beginning until /items/
-    # then cut out the stuff after the first /
-    archive_identifier = audio_link.gsub(/^.+\/items\//,'').gsub(/\/.+/,'')
   end
 end
