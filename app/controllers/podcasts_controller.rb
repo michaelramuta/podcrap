3760df7e0597f1e700ab59dae6e45e2cc0b438db
Michael Ramuta <michaelramuta@gmail.com>
2018-01-22 14:13:35 -0600

    switch asc with desc

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -17,7 +17,7 @@ class PodcastsController < ApplicationController
 
   def show
     @podcast = Podcast.find(params[:id])
-    @episodes = @podcast.user.episodes.where(live: true).order("created_at ASC")
+    @episodes = @podcast.user.episodes.where(live: true).order("created_at DESC")
     respond_to do |format|
       format.html
       format.xml
