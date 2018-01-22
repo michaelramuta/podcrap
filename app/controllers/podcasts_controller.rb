8084b85334f657c85b4b2d9b194f6b478a27fab9
Michael Ramuta <michaelramuta@gmail.com>
2018-01-22 13:08:04 -0600

    revert sorting issues

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -17,7 +17,7 @@ class PodcastsController < ApplicationController
 
   def show
     @podcast = Podcast.find(params[:id])
-    @episodes = @podcast.user.episodes.where(live: true).sort{|x, y| y.created_at <=> z.created_at }
+    @episodes = @podcast.user.episodes.where(live: true)
     respond_to do |format|
       format.html
       format.xml
